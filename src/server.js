31766d3d8f70330d90290b7205631b70a7bd6237
Joabesv <joabevarjao123@gmail.com>
2023-01-10 01:54:45 -0300

    feat(streams): speeding up the body with streams

--- src/server.js
+++ src/server.js
@@ -1,10 +1,34 @@
+// node recommends to still import;
+// https://nodejs.org/api/buffer.html#buffer
+import { Buffer } from 'node:buffer';
 import { createServer } from 'node:http';
 import { randomUUID } from 'node:crypto';
+import { Transform } from 'node:stream';
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, cb) {
+    const modifiedData = Number(chunk.toString()) * -1;
+
+    console.log(modifiedData);
+
+    cb(null, Buffer.from(String(modifiedData)));
+  }
+}
 
 const users = [];
-
-const server = createServer((req, res) => {
+const server = createServer(async (req, res) => {
   const { method, url } = req;
+  const buffers = [];
+
+  for await (const chunks of req) {
+    buffers.push(chunks);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch (err) {
+    req.body = null;
+  }
 
   if (method === 'GET' && url === '/users') {
     return res
@@ -13,10 +37,12 @@ const server = createServer((req, res) => {
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body;
+
     users.push({
       id: randomUUID(),
-      name: 'Joaozinho123',
-      email: 'joaozinhoMaisBrabo@gmail.com',
+      name,
+      email,
     });
     return res.writeHead(201).end('criando um usuário');
   }
