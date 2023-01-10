5d7563799f59ad21a3e40a6955b2b728f187a14a
Joabesv <joabevarjao123@gmail.com>
2023-01-10 00:49:12 -0300

    feat(stateful): save users in memory

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 import { createServer } from 'node:http';
+import { randomUUID } from 'node:crypto';
+
+const users = [];
 
 const server = createServer((req, res) => {
   const { method, url } = req;
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários');
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: randomUUID(),
+      name: 'Joaozinho123',
+      email: 'joaozinhoMaisBrabo@gmail.com',
+    });
+
     return res.end('criando um usuário');
   }
 
